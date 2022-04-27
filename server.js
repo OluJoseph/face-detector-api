90324cb22d2ee386959c1abd7998de65ffdbdbb9
unknown <oluwatobiloba8624@gmail.com>
2022-04-27 01:52:18 +0100

    signIn, register, update entry

--- server.js
+++ server.js
@@ -1,32 +1,22 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors')
+const cors = require('cors');
+const knex = require('knex');
+
+
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : 5432,
+      user : 'postgres',
+      password : 'Oluwatobiloba1',
+      database : 'smart-brain'
+    }
+})
 
 const app = express();
-
-const database = {
-    users: {
-        "johndoe@gmail.com": {
-            name: "John Doe",
-            email: "johndoe@gmail.com",
-            joined: new Date(),
-            password: 'cookie',
-            entries: 0,
-            id: "johndoe"
-        },
-        "olujoseph@gmail.com": {
-            name: "Olu Joseph",
-            email: "olujoseph@gmail.com",
-            joined: new Date(),
-            password: 'olu',
-            entries: 0,
-            id: "olujoseph"
-        }
-    },
-    login: {
-        
-    }  
-}
 
 app.use(cors())
 app.use(express.json())
@@ -36,15 +26,36 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
-    const lowerCaseEmail = req.body.email.toLowerCase()
-    if (database.users[lowerCaseEmail] && database.users[lowerCaseEmail].password === req.body.password){
-        res.json({
-            signIn: 'success',
-            user: database.users[lowerCaseEmail]
-        })
-    }else{
-        res.status(404).json('username or password incorrect')
-    }
+    const {email, password} = req.body;
+    const lowerCaseEmail = email.toLowerCase()
+
+    db.select('*').from('login')
+    .where({
+        email: lowerCaseEmail
+    })
+     .then(users => {
+         if(users.length > 0){
+            const isValid = bcrypt.compareSync(password, users[0].password);
+            if (isValid){
+                db.select('*').from('users').where({
+                    email: lowerCaseEmail
+                })
+                .then(data => {
+                    res.json({
+                        signIn: 'success',
+                        user: data[0]
+                    })
+                })
+                .catch(err => {
+                    res.status(400).json('error logging in')
+                })
+            }else{
+                res.status(404).json('invalid credentials')
+            }
+         }else{
+             res.status(404).json('user not found')
+         }
+     })
 })
 
 app.post('/register', (req, res) => {
@@ -52,21 +63,48 @@ app.post('/register', (req, res) => {
     const {name, email, password} = req.body;
     const lowerCaseEmail = email.toLowerCase();
 
-    if(database.users[lowerCaseEmail]) res.status(400).json('email already in use');
-
-    if (name && email && password){
-        database.users[lowerCaseEmail] = {
-            name: name,
-            email: lowerCaseEmail,
-            joined: new Date(),
-            password: password,
-            entries: 0,
-            id: lowerCaseEmail.slice(0, lowerCaseEmail.indexOf('@'))
-        };
-
-        res.json('successful')
-    }else{
-        res.status(400).json('Some fields are empty')
+    
+    if (name && email && password){//CHECK IF ALL INPUTS ARE FILLED
+        db.select('*').from('users')
+        .where({//TRY SELECT THE USER ROW IF THE USER ALREADY EXISTS
+            email: lowerCaseEmail
+        })
+        .then(userList => {
+            if(userList.length === 0){//IF USER DOESNT EXIST, ADD THE USER
+                db.transaction(trx => {
+                    trx('login')
+                    .insert({//INSERT PASSWORD HASH TO LOGIN TABLE
+                        email: lowerCaseEmail,
+                        password: bcrypt.hashSync(password)
+                    })
+                    .returning('email')
+                    .then(loginEmail => {
+                        trx('users')
+                        .insert({//INSERT USER TO USERS TABLE
+                            name: name,
+                            email: loginEmail[0].email,
+                            joined: new Date()
+                        })
+                        .returning('*')
+                        .then(user => {
+                            res.json('successful')
+                        })
+                        .catch(err => {
+                            res.json('an error occured')
+                        })
+                    })
+                    .then(trx.commit)
+                    .catch(trx.rollback)
+                })
+            }else{
+                res.status(400).json('email already exists')
+            }
+        })
+        .catch((err) => {
+            res.status(400).json('error registering user');
+        })
+    }else{//IF ONE OR TWO INPUT FIELDS ARE EMPTY
+        res.status(400).json('fields are empty')
     }
     
 })
@@ -74,29 +112,33 @@ app.post('/register', (req, res) => {
 app.get('/profile/:id', (req , res) => {
     const {id} = req.params;
 
-    for (let user in database.users) {
-        if (database.users[user].id === id) {
-            res.json(database.users[user]);
-            break;
+    db.select('*').from('users').where({id})
+    .then(user => {
+        if(user.length){
+            res.json(user[0])
+        }else{
+            res.status(404).json('user not found')
         }
-    }
-
-    res.status(404).json('not found')
-
+    })
+    .catch(err => {
+        res.status(404).json(err)
+    })
 })
 
 app.put('/image', (req , res) => {
     const { id } = req.body;
 
-    for (let user in database.users) {
-        if (database.users[user].id === id) {
-            database.users[user].entries++;
-            res.json(database.users[user])
-            break;
-        }
-    }
-    res.status(404).send('error')
-
+    db('users').where({id})
+    .increment({
+        entries: 1
+    })
+    .returning('*')
+    .then(user => {
+        res.json(user[0])
+    })
+    .catch(err => {
+        res.status(400).json("unable to update entries")
+    })
 })
 
 
